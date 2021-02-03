376f4cc05e1f08dd71b4a054f78fd0ffa5161bfe
Golapadeog <jeremy.landon@outlook.com>
2021-02-04 00:01:41 +0100

    :books: add a sample

---
+++ sample/Golap.AppleAuth.Sample/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Golap.AppleAuth.Sample
+{
+    class AppSettings
+    { 
+        public string KeyId { get; set; }
+        public string TeamId { get; set; }
+        public string ClientId { get; set; }
+        public string RedirectUri { get; set; }
+    }
+}
